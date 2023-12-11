1d8697cbd53ffb4f76ce798ae16ac20428ef8c45
KosukeArase <kosuke.arase@gmail.com>
2023-12-11 10:27:57 +0100

    fix

--- saicinpainting/training/trainers/__init__.py
+++ saicinpainting/training/trainers/__init__.py
@@ -27,9 +27,9 @@ def load_checkpoint(train_config, path, map_location='cuda', strict=True):
     state = torch.load(path, map_location=map_location)
 
     if train_config.training_model.predict_only:
-        for key in state["state_dict"].keys():
-            if "val_evaluator" in key or "test_evaluator" in key:
-                del state[key]
+        keys_to_remove = keys_to_remove = [key for key in state["state_dict"].keys() if "val_evaluator" in key or "test_evaluator" in key]
+        for key in keys_to_remove:
+            del  state["state_dict"][key]
 
     model.load_state_dict(state['state_dict'], strict=strict)
     model.on_load_checkpoint(state)
